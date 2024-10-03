fa530b019690432519478a5c8cef156eb4df5b35
LUCAS TABORDA <taborda.lucas@edu.pucrs.br>
2024-10-02 21:11:44 -0300

    init
    Init trabalho

--- README.md
+++
@@ -1,2 +0,0 @@
-# PES-T1
-Trabalho Projeto e arquitetura de software with Lucas Taborda , Gabriel Barbosa , Erick Muniz e Artur Lampert
---
+++ src/test/java/com/PAS_T1/PAS/PasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PAS_T1.PAS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
