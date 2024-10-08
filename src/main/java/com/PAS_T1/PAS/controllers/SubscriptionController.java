179c17e8b12186e197d00d726a50aab13b3f28c9
LUCAS TABORDA <lucas_rubens_95@hotmail.com>
2024-10-08 09:33:37 -0300

    refaturado

--- src/main/java/com/PAS_T1/PAS/controllers/SubscriptionController.java
+++
@@ -1,4 +0,0 @@
-package com.PAS_T1.PAS.controllers;
-
-public class SubscriptionController {
-}
--- src/main/java/com/PAS_T1/PAS/models/StatusAtivo.java
+++
@@ -1,6 +0,0 @@
-package com.PAS_T1.PAS.models;
-
-public enum StatusAtivo {
-    ACTIVE,
-    OFF;
-}
