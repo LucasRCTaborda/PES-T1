578985c0d1471e109879abc3c9277e386444f988
LUCAS TABORDA <lucas_rubens_95@hotmail.com>
2024-10-22 00:56:27 -0300

    @Renato Forte @Erick Muniz , @GabrieBarbosa ,  corrigimos os Controller

--- src/main/resources/init/data.sql
+++ src/main/resources/init/data.sql
@@ -5,22 +5,24 @@ INSERT INTO usuarios (id,login, password) VALUES (1,'admin', 'admin123');
 -- Insert de aplicativos
 INSERT INTO aplicativos (id, nome, custo_mensal) VALUES (1,'Discord', 13.50);
 INSERT INTO aplicativos (id, nome, custo_mensal) VALUES (2,'Moodle', 2.99);
-INSERT INTO aplicativos (id, nome, custo_mensal) VALUES (3,'Salsicha', 5.00);
-INSERT INTO aplicativos (id, nome, custo_mensal) VALUES (4,'Fork', 64.99);
+INSERT INTO aplicativos (id, nome, custo_mensal) VALUES (3,'Miro', 5.00);
+INSERT INTO aplicativos (id, nome, custo_mensal) VALUES (4,'CHATGPT', 64.99);
 INSERT INTO aplicativos (id, nome, custo_mensal) VALUES (5,'Xbox', 45.00);
 
 -- Insert de Clientes
 
-INSERT INTO clientes (id,nome,email) VALUES (1, 'Michael Jordan', 'maikinJord@yahoo.com.br');
-INSERT INTO clientes (id,nome,email) VALUES (2, 'Michael Kuster', 'maikaokuster@gmail.com');
-INSERT INTO clientes (id,nome,email) VALUES (3, 'Bernardo Muller', 'berndmull@bernd.com');
-INSERT INTO clientes (id,nome,email) VALUES (4, 'Francisco Lisboa', 'franlis@bernd.com');
+INSERT INTO clientes (id,nome,email) VALUES (1, 'Lucas Taborda', 'taborda.lucas@edu.pucrs.br');
+INSERT INTO clientes (id,nome,email) VALUES (2, 'Gabriel Barbosa', 'Gabriel@Barbosa.com');
+INSERT INTO clientes (id,nome,email) VALUES (3, 'Erick Muniz', 'Erick@Muniz.com');
+INSERT INTO clientes (id,nome,email) VALUES (4, 'Renato Forte', 'Renato@Forte.com');
 INSERT INTO clientes (id,nome,email) VALUES (5, 'David Campos', 'david@benrd.com');
-INSERT INTO clientes (id,nome,email) VALUES (6, 'Guilherme Barreto', 'guilh123@barreto.com');
-INSERT INTO clientes (id,nome,email) VALUES (7, 'Advogado', 'advogado@advogado.com');
-INSERT INTO clientes (id,nome,email) VALUES (8, 'Felipe Varehlla', 'felipe11@gmail.com');
-INSERT INTO clientes (id,nome,email) VALUES (9, 'Felpo Filva', 'ff1249@gmail.com');
-INSERT INTO clientes (id,nome,email) VALUES (10, 'Bernardo Copstein', 'bernardo@copstein.com');
+INSERT INTO clientes (id, nome, email) VALUES (6, 'Mariana Oliveira', 'mariana.oliveira@gmail.com');
+INSERT INTO clientes (id, nome, email) VALUES (7, 'João Pedro Martins', 'joaopedro.martins@hotmail.com');
+INSERT INTO clientes (id, nome, email) VALUES (8, 'Patrícia Silva', 'patricia_silva@uol.com.br');
+INSERT INTO clientes (id, nome, email) VALUES (9, 'André Gomes', 'andre.gomes@exemplo.com');
+INSERT INTO clientes (id, nome, email) VALUES (10, 'Rafael Torres', 'rafael_torres@yahoo.com');
+INSERT INTO clientes (id, nome, email) VALUES (11, 'Camila Ferreira', 'camila.ferreira@empresa.com');
+
 
 
 -- Insert de Assinaturas
